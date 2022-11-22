8aab9a59d826923c32a7be47565d8889564c2658
Harald-86 <79138134+Harald-86@users.noreply.github.com>
2022-11-22 12:40:07 +0100

    Updated readme

--- src/components/posts/CreatePost.jsx
+++ src/components/posts/CreatePost.jsx
@@ -11,13 +11,17 @@ import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import ValidationError from "../common/FormError";
 /* const reg =
   /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/gm; */
 
 const schema = yup.object().shape({
   title: yup.string().required("please enter tile for your shout"),
-  body: yup.string(),
+  body: yup.string().max(200),
   media: yup.string(),
+
+  /*  tags: yup.string(), */
 });
 
 export default function CreatePost() {
@@ -39,37 +43,45 @@ export default function CreatePost() {
     try {
       const entryResponse = await authenticate.post(createEntry_URL, data);
       console.log("Did i make a post?", entryResponse);
+
       e.target.reset();
     } catch (error) {
       console.log(error);
-      setEntryError(<AlertBad alert="Koble is sick, please try again later" />);
+      setEntryError();
     }
   }
 
   return (
-    <div className="form__container">
-      <Form onSubmit={handleSubmit(createEntry)}>
-        <Row>
-          <Col sm={12} md={4}>
+    <Form onSubmit={handleSubmit(createEntry)} className="entry">
+      {entryError && <ValidationError>{entryError}</ValidationError>}
+      <Row>
+        <Col sm={12} md={4}>
+          <FloatingLabel controlId="floatingTitle" label="Title" className="mb-3">
             <Form.Control name="title" placeholder="Title" {...register("title")} />
-          </Col>
+            {errors.title && <ValidationError>{errors.title.message}</ValidationError>}
+          </FloatingLabel>
+        </Col>
 
-          <Col sm={12} md={4}>
-            <Form.Control name="media" placeholder="Image Url" {...register("media")} />
-          </Col>
-        </Row>
+        <Col sm={12} md={8}>
+          <FloatingLabel controlId="floatingMedia" label="Media (Url only)" className="mb-3">
+            <Form.Control name="media" placeholder="Image Url (optional)" {...register("media")} />
+          </FloatingLabel>
+        </Col>
+      </Row>
+      <FloatingLabel controlId="floatingTextarea" label="Shout it here!" className="mb-3">
         <Form.Control
+          className="entry__textarea"
           as="textarea"
-          rows={3}
+          style={{ height: "100px" }}
+          a
           name="body"
           placeholder="My message to the KobleUnivers"
           {...register("body")}
         />
-
-        <Button type="submit" className="btn-secondary btn">
-          Shout
-        </Button>
-      </Form>
-    </div>
+      </FloatingLabel>
+      <Button type="submit" className="btn-secondary btn">
+        Shout
+      </Button>
+    </Form>
   );
 }
